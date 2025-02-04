de24104f57d6e9fec140091a2f9b3c8178683b40
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-04 09:38:18 +0700

    add route to home ("/")

---
+++ src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { TypewriterEffectSmooth } from "../components/ui/typewritter-effect";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Aceternity.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button
+          className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm cursor-pointer"
+          onClick={() => navigate("/login")}
+        >
+          Masuk
+        </button>
+        <button
+          className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm cursor-pointer"
+          onClick={() => navigate("/registrasi")}
+        >
+          Registrasi
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,17 @@ import { RouterProviderProps, createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Registrasi from "../pages/Registrasi";
 import AuthLayout from "../layouts/auth-layout";
+import Home from "../pages/Home";
 
 const router: RouterProviderProps["router"] = createBrowserRouter([
   {
     path: "/",
     element: <AuthLayout />,
     children: [
+      {
+        path: "",
+        element: <Home />,
+      },
       {
         path: "login",
         element: <Login />,
