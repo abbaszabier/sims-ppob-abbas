80a73b972132eca1b7c63b8280b363c79ad202b6
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-04 09:38:36 +0700

    slicing login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import illustrasiLogin from "../assets/Illustrasi-Login.png";
 import logo from "../assets/Logo.png";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { AtSign, LockKeyhole } from "lucide-react";
 
 export default function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -12,8 +16,18 @@ export default function Login() {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 w-full h-screen">
-      <div className="flex flex-col bg-white justify-center items-center px-8 space-y-8">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="grid grid-cols-1 sm:grid-cols-2 w-full h-screen"
+    >
+      <motion.div
+        initial={{ x: -50, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex flex-col bg-white justify-center items-center px-8 space-y-8"
+      >
         <div className="flex bg-white justify-center items-center gap-2 px-8">
           <img
             src={logo}
@@ -26,46 +40,62 @@ export default function Login() {
           Masuk atau buat akun untuk memulai
         </h2>
         <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-          <div className="mb-4">
+          <div className="relative mb-4">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <AtSign size={20} color="#adb5bd" />
+            </div>
             <input
               type="email"
               placeholder="masukan email anda"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full p-3 ps-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 text-black placeholder-[#adb5bd]"
             />
           </div>
-          <div className="mb-4 relative">
+          <div className="relative mb-4">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <LockKeyhole size={20} color="#adb5bd" />
+            </div>
             <input
               type="password"
               placeholder="masukan password anda"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full p-3 ps-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 placeholder-[#adb5bd]"
             />
           </div>
-          <button
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
             type="submit"
-            className="w-full bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 transition"
+            className="w-full bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 transition cursor-pointer"
           >
             Masuk
-          </button>
+          </motion.button>
         </form>
         <p className="text-gray-600">
           Belum punya akun? registrasi{" "}
-          <a href="#" className="text-red-500">
+          <button
+            className="text-red-500 hover:underline cursor-pointer"
+            onClick={() => navigate("/registrasi")}
+          >
             di sini
-          </a>
+          </button>
         </p>
-      </div>
+      </motion.div>
 
-      <div className="md:flex justify-center items-center h-screen">
+      <motion.div
+        initial={{ x: 50, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="md:flex justify-center items-center h-screen"
+      >
         <img
           src={illustrasiLogin}
           alt="Login Illustration"
           className="w-full h-full object-cover"
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
