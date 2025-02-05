a2bfa73e4f97e356b25cb0b97845be3549656358
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-06 00:42:14 +0700

    update axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,48 @@
 import axios from "axios";
+import { store } from "../store/store";
+import { logout } from "../features/auth/auth-slice";
+import { toast } from "react-toastify";
 
 const Axios = axios.create({
-  baseURL: "https://take-home-test-api.nutech-integrasi.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
   },
 });
+
+Axios.interceptors.request.use((config) => {
+  const state = store.getState();
+  const token = state.auth.token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      store.dispatch(logout());
+      toast.error("Sesi telah berakhir, silahkan login kembali", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default Axios;
