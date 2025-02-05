ec76ef099d36a3a684abf33b9da4b0f91243dd0d
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-02-06 00:42:05 +0700

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store.ts";
 import QueryClientProviderWrapper from "./lib/query-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProviderWrapper>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </QueryClientProviderWrapper>
   </React.StrictMode>
